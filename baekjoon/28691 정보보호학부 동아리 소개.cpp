ef18c26ce6bb96cd558eab9d8fc079cbaa840f5d
jademon <j2python@gmail.com>
2024-01-13 23:22:21 +0900

    Create 28691 정보보호학부 동아리 소개.cpp

---
+++ baekjoon/28691 정보보호학부 동아리 소개.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+int main() {
+	char c;
+	scanf("%c",&c);
+	switch(c){
+		case 'M':
+			printf("MatKor");
+			break;
+		case 'W':
+			printf("WiCys");
+			break;
+		case 'C':
+			printf("CyKor");
+			break;
+		case 'A':
+			printf("AlKor");
+			break;
+		default:
+			printf("$clear");
+			break;
+	}
+}
